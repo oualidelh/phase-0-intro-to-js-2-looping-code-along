dddbf78fd9c7ddca87e7f824d681100b1880d144
oualidelhouari <elhouarioualid@gmail.com>
2023-07-21 18:10:18 +0100

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const thankYouMessages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    }
+    return thankYouMessages;
+}
+function countDown(a) {
+    for (let i = a; i >= 0; i--) {
+
+        console.log(i);
+    }
+}
